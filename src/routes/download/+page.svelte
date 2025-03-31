17ba015326682e06ed97635419e09d2f721ca959
sebastianmicu24 <sebastianmicu24@gmail.com>
2025-03-31 16:08:37 +0200

    Updated Download Links

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -11,35 +11,35 @@
 
   <section class="mb-8 flex flex-col items-center">
     <div class="w-full max-w-md flex flex-col space-y-4">
-      <a href="https://drive.google.com/uc?export=download&id=1UZlsdjL_ryb66k6ehmDnHftVOoAvgkJx" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
+      <a href="https://drive.google.com/uc?export=download&id=1tPO8rYmKff7YlmOrUMzJ6ypn2Qc9ZdP2" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
         <span>SCHELI Plugin</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </a>
       
-      <a href="https://drive.google.com/uc?export=download&id=1nbTpHfIXLRS7tgvRnrBI3Y5rj7wUxo7L" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
+      <a href="https://drive.google.com/uc?export=download&id=1lv6AQ4EXC9sdN7A5RKYEVLHoJCZtp2SY" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
         <span>XGBoost Helper File</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </a>
       
-      <a href="https://drive.google.com/file/d/1uIO7ggsYj9-bkbnIdTVfltKU61YWMxPt/view?usp=drive_link" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
+      <a href="https://drive.google.com/uc?export=download&id=1IGETGZrsrTJHVKwt0qTeyNcthLkcJFx8" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
         <span>Manual Cell Classifier</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </a>
       
-      <a href="https://drive.google.com/uc?export=download&id=1AOfGXYE_8Qizc0sNcVRFfN-gIrtKIf9d" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
+      <a href="https://drive.google.com/uc?export=download&id=1pyjIpyV6jGaammde1AMlJQd9_GRAdsBj" class="flex items-center justify-between bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded">
         <span>Cell Visualizer</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </a>
       
-      <a href="https://drive.google.com/uc?export=download&id=1HtpmGqEXpMG4nPJNjnPjqKkJ-8PhDijs" class="flex items-center justify-between bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-6 rounded mt-2">
+      <a href="https://drive.google.com/uc?export=download&id=1tPO8rYmKff7YlmOrUMzJ6ypn2Qc9ZdP2" class="flex items-center justify-between bg-green-600 hover:bg-green-800 text-white font-bold py-3 px-6 rounded mt-2">
         <span>Download All</span>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
